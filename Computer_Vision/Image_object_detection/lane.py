31519f1907199fff7e65bb8c431496cabaf2fadb
gisellemarryme <154847718+gisellemarryme@users.noreply.github.com>
2025-03-11 10:13:37 +0900

    Update and rename lane1.py to lane.py

--- Computer_Vision/Image_object_detection/lane.py
+++ Computer_Vision/Image_object_detection/lane.py
@@ -2,36 +2,28 @@ import numpy as np
 import cv2 as cv
 import math
 
-
 img1=cv.imread('lane1.jpeg',cv.IMREAD_COLOR)
 img2=cv.imread('lane2.jpeg',cv.IMREAD_COLOR)
-
 
 if img1 is None:
     print('file load failed')
 
-
 if img2 is None:
     print('file load failed')
-
 
 gray1=cv.cvtColor(img1,cv.COLOR_BGR2GRAY)
 gray2=cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
 
-
 edge1=cv.Canny(gray1,50,150)
 lines1=cv.HoughLines(edge1,1,math.pi/180,140) #겹친 게 140개 이상인 아이만 
 
-
 edge2=cv.Canny(gray2,50,150)
 lines2=cv.HoughLines(edge2,1,math.pi/180,190)
-
 
 if lines1 is not None:
     for i in range(lines1.shape[0]):
         rho = lines1[i][0][0]
         theta = lines1[i][0][1]
-
 
         # 각도 계산
         angle = np.degrees(theta) % 180
@@ -39,7 +31,6 @@ if lines1 is not None:
         
         if 56 <= angle <= 57 or angle == 122.0:
             # print('angle', angle)  # 출력
-
 
             cos_t = math.cos(theta)
             sin_t = math.sin(theta)
@@ -49,12 +40,10 @@ if lines1 is not None:
             pt2 = (int(x0 + alpha * sin_t), int(y0 - alpha * cos_t))
             cv.line(img1, pt1, pt2, (0, 0, 255), 2, cv.LINE_AA)
 
-
 if lines2 is not None:
     for i in range(lines2.shape[0]):
         rho = lines2[i][0][0]
         theta = lines2[i][0][1]
-
 
         # 각도 계산
         angle = np.degrees(theta) % 180
@@ -62,7 +51,6 @@ if lines2 is not None:
         
         if angle == 53.999996185302734 or angle == 120.0:
             print('angle', angle)  # 출력
-
 
             cos_t = math.cos(theta)
             sin_t = math.sin(theta)
@@ -72,10 +60,14 @@ if lines2 is not None:
             pt2 = (int(x0 + alpha * sin_t), int(y0 - alpha * cos_t))
             cv.line(img2, pt1, pt2, (0, 0, 255), 2, cv.LINE_AA)
 
+desired_width = 640
+desired_height = 480
 
-cv.imshow('img1',img1)
-cv.imshow('img2',img2)
+resized_img1 = cv.resize(img1, (desired_width, desired_height))
+resized_img2 = cv.resize(img2, (desired_width, desired_height))
 
+cv.imshow('img1', resized_img1)
+cv.imshow('img2', resized_img2)
 
-cv.waitKey()
+cv.waitKey(0)
 cv.destroyAllWindows()
